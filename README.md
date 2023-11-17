6cd84c33f6eff221f385ff1ce51082157652baa4
Jon McCormack <jon.mccormack@purple.ai>
2023-11-17 15:05:27 +0000

    Readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# DS18x20 Pico MQTT
+
+An Mqtt client for the Pico that publishes DS18x20 temperature
+readings in a Home Assistant-friendly format.
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-int main() {
+int main()
+{
     setup_default_uart();
     printf("Hello, world!\n");
     return 0;
