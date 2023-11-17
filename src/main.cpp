7063ff585fa11ceae21f474d5faaca83256d49d0
Jon McCormack <jon.mccormack@purple.ai>
2023-11-17 23:25:57 +0000

    Added the pico-onewire module and a basic program which reads temp readings from the sensor. Also updated cmake config to write stdout over usb serial rather than the default serial debug interface provided by the pico

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "one_wire.h"
+#include "hardware/gpio.h"
+
+// TODO: I want a vscode task which can build the project
 
 int main()
 {
-    setup_default_uart();
-    printf("Hello, world!\n");
+    stdio_init_all();
+    One_wire one_wire(17); // GP15 - Pin 20 on Pi Pico
+    one_wire.init();
+    rom_address_t address{};
+    while (true)
+    {
+        one_wire.single_device_read_rom(address);
+        printf("Device Address: %02x%02x%02x%02x%02x%02x%02x%02x\n", address.rom[0], address.rom[1], address.rom[2], address.rom[3], address.rom[4], address.rom[5], address.rom[6], address.rom[7]);
+        one_wire.convert_temperature(address, true, false);
+        printf("Temperature: %3.1foC\n", one_wire.temperature(address));
+        sleep_ms(1000);
+    }
     return 0;
 }
