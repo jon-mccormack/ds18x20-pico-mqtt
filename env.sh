ccbb343f2bec043ab4e8e4cfe26c0578556d29de
Jon McCormack <jon.mccormack@purple.ai>
2023-11-28 22:58:23 +0000

    removed committed config file

--- env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export WIFI_SSID="28RBaulk"
-export WIFI_PASSWORD="icecream"
-export MQTT_BROKER_ADDRESS="192.168.1.13"
-export PICO_SDK_PATH="$(realpath $(dirname ${BASH_SOURCE[0]}))/modules/pico-sdk"
